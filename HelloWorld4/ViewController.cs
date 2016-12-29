9df0d918cfa6aaea5730475b1f2f0da97bbb7406
Thomas <Thomas.Speer@kabelmail.de>
2016-12-29 15:31:22 +0100

    initial version

---
+++ HelloWorld4/ViewController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace HelloWorld4
+{
+	public partial class ViewController : NSViewController
+	{
+
+		private int numberOfTimesClicked = 0;
+		public ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			this.ClickedLabel.StringValue = "not yet clicked";
+		}
+
+		public override NSObject RepresentedObject
+		{
+			get
+			{
+				return base.RepresentedObject;
+			}
+			set
+			{
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+		partial void ClickedButton(Foundation.NSObject sender)
+		{
+
+			// Update counter and label
+			ClickedLabel.StringValue = string.Format("The button has been clicked {0} time{1}.", ++numberOfTimesClicked, (numberOfTimesClicked < 2) ? "" : "s");
+		}
+	}
+}
